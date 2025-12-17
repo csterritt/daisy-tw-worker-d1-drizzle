3a31697d7ca107be18ceed263c9a4888fa6128ac
Chris Sterritt <csterritt@gmail.com>
2025-12-17 13:20:26 -0500

    Make try-reload-db.sh db independent.

--- try-reload-db.sh
+++ try-reload-db.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -euo pipefail
-sqlite-utils insert .wrangler/state/v3/d1/miniflare-D1DatabaseObject/a514711f8bc10f258a7b9292b4131ca3cb2970e09c9f377b3a348e6dbc0c3d67.sqlite account - --pk id < tmp/account_dump.json
-sqlite-utils insert .wrangler/state/v3/d1/miniflare-D1DatabaseObject/a514711f8bc10f258a7b9292b4131ca3cb2970e09c9f377b3a348e6dbc0c3d67.sqlite user - --pk id < tmp/user_dump.json
+
+if [ $(/bin/ls .wrangler/state/v3/d1/miniflare-D1DatabaseObject/*.sqlite  | wc -l) != 1 ] ; then
+  echo "Cannot find a single sqlite database in .wrangler."
+  exit 1
+fi
+DB_PATH=$(/bin/ls .wrangler/state/v3/d1/miniflare-D1DatabaseObject/*.sqlite)
+
+sqlite-utils insert $DB_PATH account - --pk id < tmp/account_dump.json
+sqlite-utils insert $DB_PATH user - --pk id < tmp/user_dump.json
